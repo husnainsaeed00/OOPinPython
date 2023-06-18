2ddc0fd4d4a446731786c73027d295e930417c1b
Husnain Saeed <husnainsaeed000@gmail.com>
2023-06-17 22:29:38 -0700

    Add files via upload

---
+++ poly.py
@@ -0,0 +1,26 @@
+class Shape:
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+    
+
+class Rectangle(Shape):
+    def __init__(self,width,height):
+        self.width = width
+        self.height =height
+
+    def area(self):
+        return (self.width*self.height)
+    
+
+circle= Circle(5)
+rect = Rectangle(45,2)
+
+print(circle.area())
+print(rect.area())
